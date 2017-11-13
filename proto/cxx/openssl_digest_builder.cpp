039529102209927f2f976d90ea73cb0e3fec6d01
dmytro.iakovliev <dmytro.iakovliev@dev.zodiac.tv>
2017-11-13 18:32:39 +0200

    Add digest calculation for abstract stream

--- proto/cxx/openssl_digest_builder.cpp
+++ proto/cxx/openssl_digest_builder.cpp
@@ -2,9 +2,13 @@
 #include <openssl/md5.h>
 
 #include <iostream>
+//#include <sstream>
 #include <vector>
 #include <algorithm>
 #include <boost/format.hpp>
+
+// Needed for test code
+#include <fstream>
 
 template<typename value_type> class DigestFormatter
 {
@@ -100,7 +104,28 @@ template<class DigestCtx> class ChecksumDigestBuilder
 public:
     ChecksumDigestBuilder() {}
 
-    typename DigestCtx::Digest digest_for(const std::string& string) const
+    typedef typename DigestCtx::Digest Digest;
+    typedef typename DigestCtx::Formatter Formatter;
+
+    Digest digest_for(std::istream& is) const
+    {
+        DigestCtx context;
+        context.init();
+        std::vector<std::istream::char_type> buf(4096); // 4k buffer
+        std::streamsize readed = 0;
+        do {
+            readed = is.read(buf.data(), buf.size()).gcount();
+            if (readed != 0) {
+                context.update(buf.data(), readed);
+            }
+        } while(!is.eof() & !is.fail() & !is.bad());
+        if (is.bad()) {
+            // TODO: There are read errors
+        }
+        return context.finalize();
+    }
+
+    Digest digest_for(const std::string& string) const
     {
         DigestCtx context;
         context.init();
@@ -111,11 +136,21 @@ public:
     std::string str_digest_for(const std::string& string) const
     {
         std::string result;
-        typename DigestCtx::Formatter resultFormatter(result);
-        typename DigestCtx::Digest digest = digest_for(string);
+        Formatter resultFormatter(result);
+        Digest digest = digest_for(string);
         std::for_each(digest.begin(), digest.end(), resultFormatter);
         return result;
     }
+
+    std::string str_digest_for(std::istream& is) const
+    {
+        std::string result;
+        Formatter resultFormatter(result);
+        Digest digest = digest_for(is);
+        std::for_each(digest.begin(), digest.end(), resultFormatter);
+        return result;
+    }
+
 };
 
 typedef ChecksumDigestBuilder<Sha256Context> Sha256DigestBuilder;
@@ -124,10 +159,16 @@ typedef ChecksumDigestBuilder<Md5Context> Md5DigestBuilder;
 
 int main(int argc, char** argv)
 {
-    std::string value = "1234";
+    std::string value = "1234\n";
+    std::string test_file = "test_file.txt";
 
     Sha256DigestBuilder builderSha256;
     std::cout << "sha-256: " << builderSha256.str_digest_for(value) << std::endl;
+
+    std::ifstream testFile(test_file.c_str(), std::ios::in|std::ios::binary);
+    if (testFile) {
+        std::cout << "sha-256 file: " << builderSha256.str_digest_for(testFile) << std::endl;
+    }
 
     ShaDigestBuilder builderSha;
     std::cout << "sha-1: " << builderSha.str_digest_for(value) << std::endl;
