18173dda25feef152786128164dd9ce62ff7598d
Oleksii Kogutenko <oleksii.kogutenko@dev.zodiac.tv>
2018-03-28 18:33:47 +0300

    added missed classes

---
+++ piel/artdeployartifactchecksumhandlers.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2017-2018
+ *
+ *  Dmytro Iakovliev daemondzk@gmail.com
+ *  Oleksii Kogutenko https://github.com/oleksii-kogutenko
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Dmytro Iakovliev daemondzk@gmail.com ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Dmytro Iakovliev daemondzk@gmail.com BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef ARTBASEAPIDEPLOYARTIFACTCHECSUMHANDLERS_H
+#define ARTBASEAPIDEPLOYARTIFACTCHECSUMHANDLERS_H
+
+#include <streamssequencepartitionallyoutputhelper.h>
+#include <checksumsdigestbuilder.hpp>
+#include <artdeployartifacthandlers.h>
+
+#include <fstream>
+#include <artbasedeployartifactshandlers.h>
+
+namespace art { namespace lib {
+
+class ArtDeployArtifactCheckSumHandlers : public ArtDeployArtifactHandlers
+{
+public:
+    ArtDeployArtifactCheckSumHandlers(const ArtDeployArtifactHandlers& handler, std::string digest);
+    virtual ~ArtDeployArtifactCheckSumHandlers();
+
+    virtual boost::shared_ptr<std::istream> prepare_header();
+    virtual size_t handle_input(char *ptr, size_t size);
+    //virtual void gen_additional_tree(boost::property_tree::ptree &);
+    virtual std::string gen_uri();
+private:
+    std::string digest_;
+};
+
+} } // namespace art::lib
+
+#endif // ARTBASEAPIDEPLOYARTIFACTCHECSUMHANDLERS_H
